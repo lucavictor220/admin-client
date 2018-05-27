6d37ecf63ebcda5461ff201ad7febbd69b5db256
Vitiok <lucavictor220@gmail.com>
2018-05-27 14:04:12 +0300

    first commit

--- app/components/App.js
+++ app/components/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
+import Button from '@material-ui/core/Button';
+import Header from './Header';
 import img from '../assets/images/react_logo_512x512.png';
+
 
 const App = () => {
   return (
     <div>
-      <h2 id="heading">Hello ReactJS</h2>
+      <Header />
+      <h2 id="heading">Hello ReactJS!!!</h2>
       <img
         className="image"
         style={{ margin: '0.5em' }}
@@ -13,6 +17,9 @@ const App = () => {
         src={img}
         alt="React Logo"
       />
+      <Button variant="raised" color="primary">
+        Hello World
+      </Button>
     </div>
   );
 };
---
+++ app/components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import AppBar from '@material-ui/core/AppBar';
+
+const Header = () => {
+  return (
+    <AppBar>
+      <span>Text</span>
+    </AppBar>
+  );
+};
+
+export default Header;
